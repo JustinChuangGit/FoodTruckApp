e740963e5ef3bde371f5bdb94a68ccf55a9df196
JustinChuangGit <80928888+JustinChuangGit@users.noreply.github.com>
2024-11-05 22:11:05 -0800

    Added a basic signup screen. Configuired Routing"

--- app/auth/LoginScreen.tsx
+++ app/auth/LoginScreen.tsx
@@ -1,16 +1,19 @@
 // app/auth/LoginScreen.tsx
+import { useRouter } from 'expo-router';
 import React from 'react';
 import { View, Text, TextInput, Button } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function LoginScreen() {
+    const router = useRouter();
   return (
-    <View className="flex-1 justify-center px-6 bg-gray-100">
+    <SafeAreaView className="flex-1 justify-center px-6 bg-gray-100">
       <Text className="text-3xl font-bold text-center mb-6 text-black">Login</Text>
 
       {/* Username Input */}
       <TextInput
         className="h-12 border border-gray-400 rounded mb-4 px-3 bg-white"
-        placeholder="Username"
+        placeholder="Email"
         placeholderTextColor="#A9A9A9" // Light gray placeholder color
         autoCapitalize="none"
       />
@@ -37,11 +40,10 @@ export default function LoginScreen() {
       <Button
         title="Sign Up"
         onPress={() => {
-          // Uncomment and replace with actual sign-up function later
-          // navigateToSignUp();
+          router.push('/auth/SignupScreen');
         }}
         color="gray"
       />
-    </View>
+    </SafeAreaView>
   );
 }
