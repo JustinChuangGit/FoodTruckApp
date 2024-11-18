2bc8ac628c25660ce4eda200df0ff6bc2919785e
JustinChuangGit <80928888+JustinChuangGit@users.noreply.github.com>
2024-11-17 21:43:27 -0800

    modified to print user home on screen to differentiate between vendor

--- app/user/UserHomeScreen.tsx
+++ app/user/UserHomeScreen.tsx
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet } from "react-native";
 export default function UserHomeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello</Text>
+      <Text style={styles.text}>User Home</Text>
     </View>
   );
 }
