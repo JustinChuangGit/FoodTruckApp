4240fd727ece8b098b61707ce62e22c8b823b923
JustinChuangGit <80928888+JustinChuangGit@users.noreply.github.com>
2024-12-18 21:40:49 -0800

    Removed The marker animation until i can figure out why it goes to  the top left when clicking the x

--- app/user/components/VendorMarker.tsx
+++ app/user/components/VendorMarker.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef } from "react";
-import { Animated, StyleSheet, View } from "react-native";
+import React from "react";
+import { StyleSheet, View } from "react-native";
 import { Marker } from "react-native-maps";
 
 interface Vendor {
@@ -12,25 +12,9 @@ interface Vendor {
 interface VendorMarkerProps {
   vendor: Vendor;
   onPress: () => void; // Callback when the marker is pressed
-  isSelected: boolean; // Whether the marker is selected
 }
 
-const VendorMarker: React.FC<VendorMarkerProps> = ({
-  vendor,
-  onPress,
-  isSelected,
-}) => {
-  const scaleAnim = useRef(new Animated.Value(1)).current; // Default scale is 1
-
-  // Animate marker size when selected or deselected
-  useEffect(() => {
-    Animated.timing(scaleAnim, {
-      toValue: isSelected ? 1.5 : 1, // Scale up if selected, normal otherwise
-      duration: 200,
-      useNativeDriver: true,
-    }).start();
-  }, [isSelected]);
-
+const VendorMarker: React.FC<VendorMarkerProps> = ({ vendor, onPress }) => {
   return (
     <Marker
       coordinate={{
@@ -39,14 +23,7 @@ const VendorMarker: React.FC<VendorMarkerProps> = ({
       }}
       onPress={onPress}
     >
-      {/* Animated marker view */}
-      <Animated.View
-        style={[styles.marker, { transform: [{ scale: scaleAnim }] }]}
-      >
-        <View
-          style={isSelected ? styles.selectedMarker : styles.defaultMarker}
-        />
-      </Animated.View>
+      <View style={styles.marker} />
     </Marker>
   );
 };
@@ -55,21 +32,9 @@ export default VendorMarker;
 
 const styles = StyleSheet.create({
   marker: {
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  defaultMarker: {
     width: 20,
     height: 20,
     borderRadius: 10,
-    backgroundColor: "white",
-  },
-  selectedMarker: {
-    width: 30,
-    height: 30,
-    borderRadius: 15,
-    backgroundColor: "orange",
-    borderWidth: 2,
-    borderColor: "white",
+    backgroundColor: "white", // Single style for all markers
   },
 });
