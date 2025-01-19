291e73fbb586c6d94e276113cdfff1eb0c75d9b1
JustinChuangGit <80928888+JustinChuangGit@users.noreply.github.com>
2025-01-18 23:12:34 -0800

    Final Styling

--- app/auth/LoginScreen.tsx
+++ app/auth/LoginScreen.tsx
@@ -3,15 +3,17 @@ import React, { useState } from "react";
 import {
   View,
   TextInput,
-  Button,
   Alert,
   Image,
   StyleSheet,
   Text,
   TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
 } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { signIn } from "../../services/auth"; // Adjust the path if necessary
+import { signIn } from "../../services/auth";
 import { useDispatch } from "react-redux";
 import { AppDispatch } from "../../redux/store";
 import { munchColors } from "@/constants/Colors";
@@ -26,10 +28,9 @@ export default function LoginScreen() {
 
   const handleLogin = async () => {
     try {
-      const user = await signIn(dispatch, email, password); // Pass dispatch
+      const user = await signIn(dispatch, email, password);
       console.log("User signed in:", user);
 
-      // Redirect based on user type
       if (user?.isVendor) {
         router.replace("/vendor");
       } else {
@@ -42,104 +43,110 @@ export default function LoginScreen() {
   };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.logoContainer}>
-        <Image
-          source={require("@/assets/images/munchLogo.png")}
-          style={styles.logo}
-          resizeMode="contain"
-        />
-      </View>
-      <View style={styles.inputContainer}>
-        <Text style={styles.header}>Login</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Email"
-          placeholderTextColor="#A9A9A9"
-          autoCapitalize="none"
-          keyboardType="email-address"
-          value={email}
-          onChangeText={setEmail}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Password"
-          placeholderTextColor="#A9A9A9"
-          secureTextEntry
-          value={password}
-          onChangeText={setPassword}
-        />
-        <View style={styles.subInputContainer}>
-          <TouchableOpacity onPress={() => {}}>
-            <Text style={styles.linkText}>Forgot Password?</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => {
-              router.push("/auth/SignupScreen");
-            }}
-          >
-            <Text style={styles.linkText}>Create an Account</Text>
-          </TouchableOpacity>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.inner}>
+          <View style={styles.logoContainer}>
+            <Image
+              source={require("@/assets/images/munchLogo.png")}
+              style={styles.logo}
+              resizeMode="contain"
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.header}>Login</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Email"
+              placeholderTextColor="#A9A9A9"
+              autoCapitalize="none"
+              keyboardType="email-address"
+              value={email}
+              onChangeText={setEmail}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Password"
+              placeholderTextColor="#A9A9A9"
+              secureTextEntry
+              value={password}
+              onChangeText={setPassword}
+            />
+            <View style={styles.subInputContainer}>
+              <TouchableOpacity onPress={() => {}}>
+                <Text style={styles.linkText}>Forgot Password?</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => {
+                  router.push("/auth/SignupScreen");
+                }}
+              >
+                <Text style={styles.linkText}>Create an Account</Text>
+              </TouchableOpacity>
+            </View>
+            <TouchableOpacity style={styles.button} onPress={handleLogin}>
+              <Text style={styles.buttonText}>Login</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-        <TouchableOpacity style={styles.button} onPress={handleLogin}>
-          <Text style={styles.buttonText}>Login</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    backgroundColor: munchColors.primary, // Orange background
+    backgroundColor: munchColors.primary,
+  },
+  inner: {
+    flex: 1,
   },
   logoContainer: {
+    flexGrow: 1, // Push the inputContainer to the bottom
     alignItems: "center",
     marginBottom: 20,
-    marginTop: 75,
+    marginTop: 100,
   },
   logo: {
     width: 300,
     height: 300,
   },
   inputContainer: {
-    flex: 1,
     backgroundColor: "white",
     borderTopLeftRadius: munchStyles.cardRadius,
     borderTopRightRadius: munchStyles.cardRadius,
     padding: 30,
-    elevation: 4, // Adds shadow for Android
-    shadowColor: "#000", // Shadow for iOS
-    shadowOffset: { width: 0, height: 2 }, // Shadow offset
-    shadowOpacity: 0.1, // Shadow opacity
-    shadowRadius: 4, // Shadow radius
+    elevation: 4,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
   },
   input: {
     height: 48,
     borderColor: "gray",
     borderWidth: 1,
     borderRadius: munchStyles.smallRadius,
-    marginBottom: 16,
+    marginTop: 20,
     paddingHorizontal: 12,
     backgroundColor: "white",
-  },
-  buttonContainer: {
-    marginBottom: 12,
   },
   header: {
     fontSize: 30,
     fontWeight: "bold",
-    marginBottom: 24,
+    marginBottom: 15,
     paddingTop: 10,
+    textAlign: "center",
   },
   subInputContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
-    marginBottom: 16,
+    marginBottom: 50,
   },
-
   button: {
     backgroundColor: munchColors.primary,
     paddingVertical: 15,
