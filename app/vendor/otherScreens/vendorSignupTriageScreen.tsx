ede4a86a3cd6ce039518db628b9b60b26046bab7
JustinChuangGit <80928888+JustinChuangGit@users.noreply.github.com>
2025-01-26 20:09:47 -0800

    Added logo

--- app/vendor/otherScreens/vendorSignupTriageScreen.tsx
+++ app/vendor/otherScreens/vendorSignupTriageScreen.tsx
@@ -1,45 +1,54 @@
 import React from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
 import { useRouter } from "expo-router";
+import { munchColors } from "@/constants/Colors";
+import { munchStyles } from "@/constants/styles";
 
 export default function VendorSignupTriageScreen() {
   const router = useRouter();
 
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Vendor Sign Up</Text>
+      <View style={styles.logoContainer}>
+        <Image
+          source={require("@/assets/images/munchLogo.png")}
+          style={styles.logo}
+          resizeMode="contain"
+        />
+      </View>
+      <View style={styles.triageContainer}>
+        <TouchableOpacity
+          style={styles.row}
+          onPress={() =>
+            router.push("/vendor/otherScreens/vendorEditAccountScreen")
+          }
+        >
+          <Text style={styles.rowText}>Add Account Details</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.row}
+          onPress={() =>
+            router.push("/vendor/otherScreens/vendorEditMenuScreen")
+          }
+        >
+          <Text style={styles.rowText}>Add Menu Items</Text>
+        </TouchableOpacity>
 
-      {/* Triage Options */}
+        <TouchableOpacity
+          style={styles.row}
+          onPress={() => router.push("/sharedScreens/termsAndServiceScreen")}
+        >
+          <Text style={styles.rowText}>Terms and Conditions</Text>
+        </TouchableOpacity>
 
-      <TouchableOpacity
-        style={styles.row}
-        onPress={() =>
-          router.push("/vendor/otherScreens/vendorEditAccountScreen")
-        }
-      >
-        <Text style={styles.rowText}>Edit Account</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.row}
-        onPress={() => router.push("/vendor/otherScreens/vendorEditMenuScreen")}
-      >
-        <Text style={styles.rowText}>Edit Menu</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity
-        style={styles.row}
-        onPress={() => router.push("/sharedScreens/termsAndServiceScreen")}
-      >
-        <Text style={styles.rowText}>Terms and Conditions</Text>
-      </TouchableOpacity>
-
-      {/* Sign Up Button */}
-      <TouchableOpacity
-        style={styles.buttonContainer}
-        onPress={() => router.push("/vendor")}
-      >
-        <Text>Sign Up</Text>
-      </TouchableOpacity>
+        {/* Sign Up Button */}
+        <TouchableOpacity
+          style={styles.buttonContainer}
+          onPress={() => router.push("/vendor")}
+        >
+          <Text style={styles.buttonText}>Sign Up</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -47,7 +56,7 @@ export default function VendorSignupTriageScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#f8f8f8",
+    backgroundColor: munchColors.primary,
     padding: 16,
     justifyContent: "center",
   },
@@ -67,6 +76,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.1,
     shadowRadius: 4,
     elevation: 2,
+    height: 100,
   },
   rowText: {
     fontSize: 18,
@@ -76,5 +86,26 @@ const styles = StyleSheet.create({
   buttonContainer: {
     marginTop: 32,
     alignItems: "center",
+    backgroundColor: "white",
+    height: 48,
+    borderRadius: munchStyles.smallRadius,
+    elevation: 5,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    justifyContent: "center",
   },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: munchColors.primary,
+  },
+  logoContainer: {
+    alignItems: "center",
+    marginBottom: 50,
+  },
+  logo: {
+    width: 200,
+    height: 200,
+  },
+  triageContainer: {},
 });
